d8be925acc62e5f55087ba70bd22a3b64a3cd13e
dac <dac@dypm.org>
2023-04-27 20:00:08 +0530

    CoreJavaDay1

---
+++ day1.1/src/com/app/fruits/Mango.java
@@ -0,0 +1,22 @@
+package com.app.fruits;
+
+public class Mango extends Fruit {
+
+	
+	public Mango(String name, double weight, String color) {
+		super(name, weight, color);
+	}
+	// Mango : should return  "sweet"
+
+	@Override
+	public String taste() {
+		return  "sweet";
+	}
+	// In Mango : public void pulp() {Display name n color of the fruit + a mesg  creating  pulp!}
+public void pulp() {
+	System.out.println("Name=" +getName());
+	System.out.println("Color=" +getColor());
+	System.out.println("creating pulp");
+	
+}
+}
